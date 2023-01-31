66294179b2bac65753eb9080bc8e38fcb5b0e2ba
Kübra Kılıç <69002604+kubrakll@users.noreply.github.com>
2023-01-31 21:25:58 +0300

    Entitylerin Oluşturulması

--- CoreProject/notlar.cs
+++ CoreProject/notlar.cs
@@ -12,6 +12,14 @@ namespace CoreProject
          * Entity Layer, Data Access Layer, Business Layer, Presentation Layer, Core Layer, Api
          * Solution üzerinde sağ tık > new project > class libray 
          * 
+         * Adım: Entitylerin Oluşturulur.
+         * Makale, Kategori, Yorumlar, Yazarlar, Hakkımda, İletişim, Adress, 
+         * Önce Concrete klasörü oluşturulur ve doldurulur.
+         * 
+         * Entity Framework Core Paketleri Yüklenmesi bunlar; Data Access > Manage > (Entity Framework Core + Entity Framework Core.Design + Entity Framework Core.SqlServer + Entity Framework Core.Tools
+         * 
+         * Adım: Data Access de klasörlerin oluşturulması. Concrete, Context, Abstract, Repositories
+         * 
          */
     }
 }
